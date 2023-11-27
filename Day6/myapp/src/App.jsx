d905a9198bf04270d6dd7f969aac58c637f201e8
Ayushsahu <ayushsahupip@gmail.com>
2023-11-27 17:09:28 +0530

    Day6_React

---
+++ Day6/myapp/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  const name = "boss"
+  const name1 = "boss"
+  return (
+    <div>
+      <h1> My name is {787+888}</h1>
+      <h1> My name is {name1}</h1>
+      Lorem, ipsum dolor.
+    </div>
+  )
+}
+
+export default App
