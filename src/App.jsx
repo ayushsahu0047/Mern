dcda12f2d5cdcff3c585190e179b4f54d7916376
ayushsahu0047 <141476520+ayushsahu0047@users.noreply.github.com>
2023-11-28 16:59:22 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Person from "./components/Person"
+import Addhar from './components/Addhar'
+
+const App = () => {
+  return (
+    <div className='superman'>
+
+    
+    <Person/>
+    </div>
+
+  )
+}
+
+export default App
