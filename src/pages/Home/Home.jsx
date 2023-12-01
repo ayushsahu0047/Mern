bf0044af713b7f6b59ad87ebeafd9ede6c1de609
sagar samvedi <sagarsamvedi31@gmail.com>
2023-12-01 18:31:06 +0530

    modified name of home.css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "./home.css";
+import "./Home.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import { Link } from "react-router-dom";
