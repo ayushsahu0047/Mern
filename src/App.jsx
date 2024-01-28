ab9ca3abcd2597231fb4eb8fc80b9b2895609159
ayushsahu0047 <141476520+ayushsahu0047@users.noreply.github.com>
2024-01-28 22:14:33 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,55 @@
-import React, { useState } from 'react'
-import Form from './Form'
-import Todos from './Todo'
+import React, { useState,useCallback, useEffect, useRef } from 'react'
+
 const App = () => {
-  const [todos, setTodos] = useState([
+  const [length, setlength] = useState(8)
+  const [numberAllowed, setnumberAllowed] = useState(false)
+  const [charAllowed, setcharAllowed] = useState(false)
+  const [password, setpassword] = useState("")
+
+  //Use Ref
+  const passwordRef = useRef(null)
+
+
+  const passwordgenerate =  useCallback(() => {
+    let pass1 = "" 
+    let str = "ABCDEFGHIGKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" 
+    if(numberAllowed) str += "0123456789"
+
+    for (let i =1;i<=length;i++)
     {
-      id:1,
-      title:"This is brand new Title",
-      description:"This is brand new Description"
-    },
-    {
-      id:2,
-      title:"This is brand new Title 2",
-      description:"This is brand new Description 2"
-    },
-    {
-      id:3,
-      title:"This is brand new Title 3",
-      description:"This is brand new Description 3."
-    },
-  ])
+    let char = Math.floor(Math.random()*str.length + 1)
+    pass1 += str.charAt(char)
+    }
+    setpassword(pass1)
 
-  const [id, setId] = useState("")
+  },[length,numberAllowed,charAllowed,setpassword])
 
-  console.log("This is id = ",id)
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
 
-  const deleteTodo = (id) =>{
-    setTodos(todos.filter((d)=>d.id != id))
-  }
-
-
+  useEffect(()=>{
+    passwordgenerate()
+  },[length,numberAllowed,charAllowed,passwordgenerate])
+  
   return (
-<>
+    <> 
+    <div className='back1'>
+      <h1>Password Generator</h1>
+       <input type="text"  value={password} 
+       placeholder='Password' readOnly ref={passwordRef}/>
+       <button className='btn' onClick={copyPasswordToClipboard}>Copy</button>
+       <input type="range" min={6} max={100} value={length} onChange={(e) =>{setlength(e.target.value)}} />
+       
+       <h1>Length:{length}</h1>
+       <input type="checkbox" defaultChecked={numberAllowed} id='numberInput' onChange={()=> setnumberAllowed((prev)=>!prev)} />
+       <h1 htmlFor="characterInput">Numbers</h1>
 
-<div className="container">
-  <h1 className='text-center'>React To do List CRUD App</h1>
+       </div>
+  </>
 
-
-{/* <button
-onClick={()=>deleteTodo(2)} 
- className='btn btn-danger'>Test Delete</button> */}
-
-  <Form
-    id={id}
-    setId={setId}
-    todos = {todos} 
-    setTodos={setTodos}
-     />
-  <Todos  todos={todos} deleteTodo={deleteTodo} setId={setId} />
-</div>
-
-</>
   )
 }
 
